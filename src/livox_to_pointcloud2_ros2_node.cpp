3eb20ea89c9065e8e75934b146042c82918f0cf4
nvidia <saichan8@seas.upenn.edu>
2025-01-17 13:37:57 -0500

    modified codes to include only ROS2 stuff

--- src/livox_to_pointcloud2_ros2_node.cpp
+++ src/livox_to_pointcloud2_ros2_node.cpp
@@ -2,9 +2,10 @@
 #include <rclcpp/rclcpp.hpp>
 #include <livox_to_pointcloud2/livox_to_pointcloud2_ros2.hpp>
 
-int main(int argc, char** argv) {
-  rclcpp::init(argc, argv);
-  rclcpp::NodeOptions options;
-  rclcpp::spin(std::make_shared<livox_to_pointcloud2::LivoxToPointCloud2>(options));
-  return 0;
+int main(int argc, char** argv) 
+{
+    rclcpp::init(argc, argv);
+    rclcpp::NodeOptions options;
+    rclcpp::spin(std::make_shared<livox_to_pointcloud2::LivoxToPointCloud2>(options));
+    return 0;
 }
